ace15e1d827dd000d9e13b95b0b4a28aeba0b834
Simon Eames <simon.richard.eames@gmail.com>
2013-08-17 09:40:09 -0700

    testing and karma setup

---
+++ karma.conf.js
@@ -0,0 +1,18 @@
+basePath = '';
+files = [
+  JASMINE,
+  JASMINE_ADAPTER,
+  'js/vendor/angular.min.js',
+  'js/public/**/*.js',
+  'test/js/*.js',
+];
+autoWatch      = true;
+//browsers       = ['Chrome', 'ChromeCanary', 'Firefox', 'Opera', 'Safari', 'PhantomJS', 'IE']
+browsers       = ['PhantomJS'];
+captureTimeout = 5000;
+colors         = true;
+logLevel       = LOG_INFO; // possible values: LOG_DISABLE || LOG_ERROR || LOG_WARN || LOG_INFO || LOG_DEBUG
+port           = 9876;
+reporters      = ['progress'];
+runnerPort     = 9100;
+singleRun      = false;
---
+++ test/js/app.spec.js
@@ -0,0 +1,7 @@
+describe("app: ", function() {
+
+  it("runs the test runner", function() {
+    expect(undefined).not.toBeDefined();
+  });
+
+});// describe app
