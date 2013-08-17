cd1c27ebc29d56ef82dded59a4a519421b520b49
Simon Eames <simon.richard.eames@gmail.com>
2013-08-17 14:18:21 -0700

    controller with filter

--- js/public/app.js
+++ js/public/app.js
@@ -2,28 +2,57 @@
 
 angular.module('services', []);
 angular.module('services').factory('DataService', ['$rootScope', function($rootScope) {
-  var commands = [];
-  var service = {};
+    var commands = [];
+    var service = {};
+    service.init = function() {
+    };
 
-  service.getCommands = function() {
-    return commands;
-  };
-  service.addCommand = function(item) {
-   var itemArray = _.isArray(item) ? item : [item];
-    commands = _.chain(commands)
-      .union(itemArray)
-      .without(undefined)
-      .value();
-    return commands;
-  };
-  service.removeCommand = function(item) {
-    commands = _.without(commands, item);
-    return commands;
-  };
+    service.getCommands = function() {
+        return commands;
+    };
+    service.addCommand = function(item) {
+        var itemArray = _.isArray(item) ? item : [item];
+        commands = _.chain(commands)
+            .union(itemArray)
+            .without(undefined)
+            .value();
+        return commands;
+    };
+    service.removeCommand = function(item) {
+        commands = _.without(commands, item);
+        return commands;
+    };
 
-  return service;
+    service.init();
+    return service;
 }]); //  services.command
 
 angular.module('controllers', ['services']);
 angular.module('controllers').controller('CommandCtrl', ['$scope', 'DataService', function($scope, DataService) {
+    $scope.all = [];
+    $scope.filtered = [];
+    $scope.filter = "";
+
+    $scope.$watch("filter", function(newVal, oldval) {
+        if (newVal === oldval) {
+            //do nothing
+        }
+        $scope.filtered = _.filter($scope.all, function(item) {
+            var filterItems = $scope.filter.split(' ');
+            var match = _.find(filterItems, function(f){
+                return -1 !== item.name.indexOf(f) ||
+                       -1 !== item.description.indexOf(f);
+            });
+            return !_.isUndefined(match);
+        });
+    });
+
+    var init = function() {
+        $scope.all = DataService.getCommands();
+        $scope.filtered = $scope.all;
+    }
+    $scope.init = function() {
+        init();
+    };
+
 }]);
--- test/js/app.spec.js
+++ test/js/app.spec.js
@@ -1,136 +1,193 @@
 'use strict()';
 
 describe("app: ", function() {
-  it("runs the test runner", function() {
-    expect(undefined).not.toBeDefined();
-  });
-
-  describe("dal: ", function() {
-    var $scope;
-    var DataService;
-
-    beforeEach(function() {
-      module('services');
-      inject(function(_$rootScope_, _DataService_) {
-        $scope = _$rootScope_.$new();
-        DataService = _DataService_;
-      });
+    it("runs the test runner", function() {
+        expect(undefined).not.toBeDefined();
     });
 
-    describe("test setup: ", function() {
-      it('injects $scope', function () {
-        expect($scope).toBeDefined();
-      });
-      it('injects DataService', function () {
-        expect(DataService).toBeDefined();
-      });
-    }); // test setup
+    describe("dal: ", function() {
+        var $scope;
+        var DataService;
 
-    describe("DataService: ", function() {
-      it('loads with expected methods', function () {
-        expect(DataService.getCommands).toBeDefined();
-        expect(DataService.addCommand).toBeDefined();
-        expect(DataService.removeCommand).toBeDefined();
-      });
+        beforeEach(function() {
+            module('services');
+            inject(function(_$rootScope_, _DataService_) {
+                $scope = _$rootScope_.$new();
+                DataService = _DataService_;
+            });
+        });
 
-      it('gets command list', function () {
-        expect(DataService.getCommands()).toEqual([]);
-      });
-      it('add returns commands', function () {
-        var cmd = { name: 'name' };
-        expect(DataService.addCommand( cmd )).toEqual([cmd]);
-      });
-      it('add', function () {
-        var cmd = { name: 'name' };
-        expect(DataService.addCommand( cmd )).toEqual([cmd]);
-      });
-      it('adds without duplicates', function () {
-        var cmd = { name: 'name' };
-        expect(DataService.addCommand( cmd )).toEqual([cmd]);
-      });
-      it('adds without duplicates', function () {
-        var cmd = { name: 'name' };
-        DataService.addCommand( cmd );
-        expect(DataService.addCommand( cmd )).toEqual([cmd]);
-      });
-      it('adds a list', function () {
-        var c1 = { name: '1' };
-        var c2 = { name: '2' };
-        expect(DataService.addCommand([c1, c2])).toEqual([c1, c2]);
-      });
-      it('adds a list with concat', function () {
-        var c1 = { name: '1' };
-        var c2 = { name: '2' };
-        DataService.addCommand([c1]);
-        expect(DataService.addCommand([c2])).toEqual([c1, c2]);
-      });
-      it('adds a list with with uniques', function () {
-        var c1 = { name: '1' };
-        var c2 = { name: '2' };
-        DataService.addCommand([c1]);
-        expect(DataService.addCommand([c1, c2])).toEqual([c1, c2]);
-      });
-      it('adds a list with without duplicates', function () {
-        var c1 = { name: '1' };
-        expect(DataService.addCommand([c1, c1])).toEqual([c1]);
-      });
-      it('does not add undefined values', function () {
-        expect(DataService.addCommand(undefined)).toEqual([]);
-      });
+        describe("test setup: ", function() {
+            it('injects $scope', function () {
+                expect($scope).toBeDefined();
+            });
+            it('injects DataService', function () {
+                expect(DataService).toBeDefined();
+            });
+        }); // test setup
 
-      it('removes returns commands', function () {
-        var c1 = { name: '1' };
-        var c2 = { name: '2' };
-        DataService.addCommand([c1, c2]);
-        expect(DataService.removeCommand(c1)).toEqual([c2]);
-      });
-      it('removes', function () {
-        var c1 = { name: '1' };
-        var c2 = { name: '2' };
-        DataService.addCommand([c1, c2]);
-        expect(DataService.removeCommand(c1)).toEqual([c2]);
-      });
-      it('remove ignores undefined', function () {
-        expect(DataService.removeCommand(undefined)).toEqual([]);
-      });
-      it('remove ignores non existent', function () {
-        var c1 = { name: '1' };
-        expect(DataService.removeCommand(c1)).toEqual([]);
-      });
+        describe("DataService: ", function() {
 
-    });
+            it('loads with expected methods', function () {
+                expect(DataService.getCommands).toBeDefined();
+                expect(DataService.addCommand).toBeDefined();
+                expect(DataService.removeCommand).toBeDefined();
+            });
 
-  }); // dal
+            it('gets command list', function () {
+                expect(DataService.getCommands()).toEqual([]);
+            });
+            it('add returns commands', function () {
+                var cmd = { name: 'name' };
+                expect(DataService.addCommand( cmd )).toEqual([cmd]);
+            });
+            it('add', function () {
+                var cmd = { name: 'name' };
+                expect(DataService.addCommand( cmd )).toEqual([cmd]);
+            });
+            it('adds without duplicates', function () {
+                var cmd = { name: 'name' };
+                expect(DataService.addCommand( cmd )).toEqual([cmd]);
+            });
+            it('adds without duplicates', function () {
+                var cmd = { name: 'name' };
+                DataService.addCommand( cmd );
+                expect(DataService.addCommand( cmd )).toEqual([cmd]);
+            });
+            it('adds a list', function () {
+                var c1 = { name: '1' };
+                var c2 = { name: '2' };
+                expect(DataService.addCommand([c1, c2])).toEqual([c1, c2]);
+            });
+            it('adds a list with concat', function () {
+                var c1 = { name: '1' };
+                var c2 = { name: '2' };
+                DataService.addCommand([c1]);
+                expect(DataService.addCommand([c2])).toEqual([c1, c2]);
+            });
+            it('adds a list with with uniques', function () {
+                var c1 = { name: '1' };
+                var c2 = { name: '2' };
+                DataService.addCommand([c1]);
+                expect(DataService.addCommand([c1, c2])).toEqual([c1, c2]);
+            });
+            it('adds a list with without duplicates', function () {
+                var c1 = { name: '1' };
+                expect(DataService.addCommand([c1, c1])).toEqual([c1]);
+            });
+            it('does not add undefined values', function () {
+                expect(DataService.addCommand(undefined)).toEqual([]);
+            });
+
+            it('removes returns commands', function () {
+                var c1 = { name: '1' };
+                var c2 = { name: '2' };
+                DataService.addCommand([c1, c2]);
+                expect(DataService.removeCommand(c1)).toEqual([c2]);
+            });
+            it('removes', function () {
+                var c1 = { name: '1' };
+                var c2 = { name: '2' };
+                DataService.addCommand([c1, c2]);
+                expect(DataService.removeCommand(c1)).toEqual([c2]);
+            });
+            it('remove ignores undefined', function () {
+                expect(DataService.removeCommand(undefined)).toEqual([]);
+            });
+            it('remove ignores non existent', function () {
+                var c1 = { name: '1' };
+                expect(DataService.removeCommand(c1)).toEqual([]);
+            });
+
+        });
+
+    }); // dal
 
 
-  describe('CommandController: ', function () {
-    var $scope;
-    var $controller;
-    var $ctrl;
+    ddescribe('CommandController: ', function () {
+        var $scope;
+        var $controller;
+        var $ctrl;
 
-    beforeEach(function() {
-      module('controllers');
-      inject(function(_$rootScope_ ) {
-        $scope = _$rootScope_.$new();
-      });
-    });
-    var setupScope = function() {
-      inject(function($controller) {
-        $ctrl = $controller('CommandCtrl', {$scope:$scope});
-      });
-    }
+        var createCmd = function(name, desc) {
+            return { name: name, description: desc };
+        }
+        var testCmds = function() {
+            return [ createCmd("AAAAAA", "AAAAAA description"),
+                     createCmd("BBBBBB", "BBBBBB description"),
+                     createCmd("CCCCCC", "CCCCCC description"),
+                     createCmd("ABCABC", "EEEEEE description"), ]
+        };
 
-    describe("test setup: ", function() {
-      beforeEach( function() {
-        setupScope();
-      });
-      it('injects $scope', function () {
-        expect($scope).toBeDefined();
-      });
-      it('injects DataService', function () {
-        expect($ctrl).toBeDefined();
-      });
-    }); // test setup
+        beforeEach(function() {
+            module('controllers');
+            inject(function(_$rootScope_, $controller, _DataService_) {
+                $scope = _$rootScope_.$new();
+                var dataService = _DataService_;
 
-  }); // command controller
+                $ctrl = $controller('CommandCtrl', {
+                    $scope:$scope,
+                    DataService: dataService
+                });
+
+                spyOn(dataService, 'getCommands').andReturn(testCmds());
+
+            });
+        });
+
+        describe("test setup: ", function() {
+            it('injects $scope', function () {
+                expect($scope).toBeDefined();
+            });
+            it('injects DataService', function () {
+                expect($ctrl).toBeDefined();
+            });
+        }); // test setup
+
+        it('has a list of commands', function(){
+            $scope.init();
+            expect($scope.all).toEqual(testCmds());
+        });
+        it('has a list of filtered commands', function(){
+            $scope.init();
+            expect($scope.filtered).toEqual(testCmds());
+        });
+        it('has a filter that start empty', function(){
+            $scope.init();
+            expect($scope.filter).toBe("");
+        });
+        it('has a filter that filters on exact name', function(){
+            $scope.init();
+            $scope.filter = "AAAAAA";
+            $scope.$apply();
+            expect($scope.filtered).toEqual([createCmd("AAAAAA", "AAAAAA description")]);
+        });
+        it('has a filter that filters on partial name', function(){
+            $scope.init();
+            $scope.filter = "AAAAA";
+            $scope.$apply();
+            expect($scope.filtered).toEqual([createCmd("AAAAAA", "AAAAAA description")]);
+        });
+        it('has a filter that filters on any value in name', function(){
+            $scope.init();
+            $scope.filter = "AAAAAA BBBBBB";
+            $scope.$apply();
+            expect($scope.filtered).toEqual([createCmd("AAAAAA", "AAAAAA description"),
+                                             createCmd("BBBBBB", "BBBBBB description")]);
+        });
+        it('has a filter that filters on description', function(){
+            $scope.init();
+            $scope.filter = "EEEEEE";
+            $scope.$apply();
+            expect($scope.filtered).toEqual([createCmd("ABCABC", "EEEEEE description")]);
+        });
+        it('has a filter that filters on nameor description', function(){
+            $scope.init();
+            $scope.filter = "AAAAAA EEEEEE";
+            $scope.$apply();
+            expect($scope.filtered).toEqual([createCmd("AAAAAA", "AAAAAA description"),
+                                             createCmd("ABCABC", "EEEEEE description")]);
+        });
+
+    }); // command controller
 });// describe app
